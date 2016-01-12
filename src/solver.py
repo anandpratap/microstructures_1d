31339b9ff5e4913342a18de4ed9b39be4d588f96
Anand <anandps@umich.edu>
2016-01-11 20:24:40 -0500

    - Added non uniform grid derivatives - Works with uniform grid, non uniform grid has to be tested, some weird issues encountered during testing with sin(x)

--- src/solver.py
+++ src/solver.py
@@ -10,7 +10,6 @@ class Solver(object):
         self.x = np.copy(x)
         self.u = np.copy(u)
         self.n = np.size(self.u)
-        self.dx = x[1] - x[0]
         assert(np.size(x) == np.size(u) == self.n)
 
         self.u_x = np.zeros(self.n, dtype=self.x.dtype)
@@ -27,26 +26,154 @@ class Solver(object):
         self.hbc = True
 
     def calc_first_der(self, u):
-        self.u_x[1:-1] = (u[2:] - u[0:-2])/(2*self.dx)
+        x = self.x
+        self.u_x[1:-1] = (u[2:] - u[0:-2])/(x[2:] - x[0:-2])
+        self.u_x[0] = (u[0] - u[1])/(x[0] - x[1])
+        self.u_x[-1] = (u[-1] - u[-2])/(x[-1] - x[-2])
+        return self.u_x
     
     def calc_second_der(self, u):
-        self.u_xx[1:-1] = (u[2:] - 2*u[1:-1] + u[0:-2])/self.dx**2
+        deta = 1.0
+        x = self.x
+        nx = np.size(x)
+        x_eta = np.zeros(nx, dtype = np.complex)
+        u_eta2 = np.zeros(nx, dtype = np.complex)
+        x_eta2 = np.zeros(nx, dtype = np.complex)
+        
+        x_eta2[1:nx-1] = (x[2:nx] - 2.*x[1:nx-1] + x[0:nx-2])/deta**2
+        u_eta2[1:nx-1] = (u[2:nx] - 2.*u[1:nx-1] + u[0:nx-2])/deta**2
+        x_eta[1:nx-1] = (x[2:nx] - x[0:nx-2])/(2.*deta)
+        
+        x_eta2[0] = (x[0] - 2.0*x[1] + x[2])/deta**2
+        u_eta2[0] = (u[0] - 2.0*u[1] + u[2])/deta**2
+        x_eta[0] = (x[1] - x[0])/deta
+        
+        x_eta2[-1] = (x[-1] - 2.0*x[-2] + x[-3])/deta**2
+        u_eta2[-1] = (u[-1] - 2.0*u[-2] + u[-3])/deta**2
+        x_eta[-1] = (x[-1] - x[-2])/deta
+        u_x = self.calc_first_der(u)
+        
+        from pylab import *
+        subplot(211)
+        plot(x, (u_x*x_eta2 - u_eta2), 'ro')
+        subplot(212)
+        plot(x, 1/x_eta**2, 'ro-')
+        # #plot(x, u_x*x_eta2, 'ro')
+        show()
+        u_xx = -(u_x*x_eta2 - u_eta2)/(x_eta**2 + 1.e-16)
+        self.u_xx[:] = u_xx[:]
+        return self.u_xx
         
     def calc_third_der(self, u):
-        raise NotImplementedError("Third derivative is not available!")
+        deta = 1.0
+        x = self.x
+        nx = np.size(x)
+        x_eta = np.zeros(nx, dtype = np.complex)
+        u_eta2 = np.zeros(nx, dtype = np.complex)
+        x_eta2 = np.zeros(nx, dtype = np.complex)
+        x_eta3 = np.zeros(nx, dtype = np.complex)
+        u_eta3 = np.zeros(nx, dtype = np.complex)
+        u_x = self.calc_first_der(u)
         
-    def calc_fourth_der(self, u):
-        self.u_xxxx[2:-2] = (u[0:-4] - 4.0*u[1:-3] + 6.0*u[2:-2] - 4.0*u[3:-1] + u[4:])/self.dx**4
+        x_eta2[1:nx-1] = (x[2:nx] - 2.*x[1:nx-1] + x[0:nx-2])/deta**2
+        u_eta2[1:nx-1] = (u[2:nx] - 2.*u[1:nx-1] + u[0:nx-2])/deta**2
+        x_eta[1:nx-1] = (x[2:nx] - x[0:nx-2])/(2.*deta)
+        
+        x_eta2[0] = (x[0] - 2.0*x[1] + x[2])/deta**2
+        u_eta2[0] = (u[0] - 2.0*u[1] + u[2])/deta**2
+        x_eta[0] = (x[1] - x[0])/deta
+        
+        x_eta2[-1] = (x[-1] - 2.0*x[-2] + x[-3])/deta**2
+        u_eta2[-1] = (u[-1] - 2.0*u[-2] + u[-3])/deta**2
+        x_eta[-1] = (x[-1] - x[-2])/deta
+
+        u_eta3[2:-2] = (-0.5*u[0:-4] + u[1:-3] - u[3:-1] + 0.5*u[4:])/deta**3
         if self.hbc:
             ub = 4.0*(-5.0/6.0*u[0] + 1.5*u[1] - 0.5*u[2] + 1.0/12.0*u[3])
         else:
             ub = u[1]
-        self.u_xxxx[1] = (ub - 4.0*u[0] + 6.0*u[1] - 4.0*u[2] + u[3])/self.dx**4
+
+        u_eta3[1] = (-0.5*ub + u[0] - u[2] + 0.5*u[3])/deta**3
         if self.hbc:
             ub = -4.0*(5.0/6.0*u[-1] - 1.5*u[-2] + 0.5*u[-3] - 1.0/12.0*u[-4])
         else:
             ub = u[-2]
-        self.u_xxxx[-2] = (u[-4] - 4.0*u[-3] + 6.0*u[-2] - 4.0*u[-1] + ub)/self.dx**4
+        u_eta3[-2] = (-0.5*u[-4] + u[-3] - u[-1] + 0.5*ub)/deta**4
+        
+        x_eta3[2:-2] = (-0.5*x[0:-4] + x[1:-3] - x[3:-1] + 0.5*x[4:])/deta**3
+        x_eta3[1] = -2.5*x[1] + 9.0*x[2] - 12.0*x[3] + 7.0*x[4] - 1.5*x[5] 
+        x_eta3[-2] = 2.5*x[-2] - 9.0*x[-3] + 12.0*x[-4] - 7.0*x[-5] + 1.5*x[-6] 
+        
+        u_x = self.calc_first_der(u)
+        u_xx = self.calc_second_der(u)
+        u_xxx = -(u_x*x_eta3 + 3.0*u_xx*x_eta*x_eta2 - u_eta3)/(x_eta**3 + 1e-16)
+        self.u_xxx[1:-1] = u_xxx[1:-1]
+        return self.u_xxx
+                
+    def calc_fourth_der(self, u):
+        deta = 1.0
+        x = self.x
+        nx = np.size(x)
+        x_eta = np.zeros(nx, dtype = np.complex)
+        u_eta2 = np.zeros(nx, dtype = np.complex)
+        x_eta2 = np.zeros(nx, dtype = np.complex)
+        x_eta3 = np.zeros(nx, dtype = np.complex)
+        u_eta3 = np.zeros(nx, dtype = np.complex)
+        x_eta4 = np.zeros(nx, dtype = np.complex)
+        u_eta4 = np.zeros(nx, dtype = np.complex)
+        u_x = self.calc_first_der(u)
+        
+        x_eta2[1:nx-1] = (x[2:nx] - 2.*x[1:nx-1] + x[0:nx-2])/deta**2
+        u_eta2[1:nx-1] = (u[2:nx] - 2.*u[1:nx-1] + u[0:nx-2])/deta**2
+        x_eta[1:nx-1] = (x[2:nx] - x[0:nx-2])/(2.*deta)
+        
+        x_eta2[0] = (x[0] - 2.0*x[1] + x[2])/deta**2
+        u_eta2[0] = (u[0] - 2.0*u[1] + u[2])/deta**2
+        x_eta[0] = (x[1] - x[0])/deta
+        
+        x_eta2[-1] = (x[-1] - 2.0*x[-2] + x[-3])/deta**2
+        u_eta2[-1] = (u[-1] - 2.0*u[-2] + u[-3])/deta**2
+        x_eta[-1] = (x[-1] - x[-2])/deta
+
+        u_eta3[2:-2] = (-0.5*u[0:-4] + u[1:-3] - u[3:-1] + 0.5*u[4:])/deta**3
+        if self.hbc:
+            ub = 4.0*(-5.0/6.0*u[0] + 1.5*u[1] - 0.5*u[2] + 1.0/12.0*u[3])
+        else:
+            ub = u[1]
+
+        u_eta3[1] = (-0.5*ub + u[0] - u[2] + 0.5*u[3])/deta**3
+        if self.hbc:
+            ub = -4.0*(5.0/6.0*u[-1] - 1.5*u[-2] + 0.5*u[-3] - 1.0/12.0*u[-4])
+        else:
+            ub = u[-2]
+        u_eta3[-2] = (-0.5*u[-4] + u[-3] - u[-1] + 0.5*ub)/deta**4
+        
+        x_eta3[2:-2] = (-0.5*x[0:-4] + x[1:-3] - x[3:-1] + 0.5*x[4:])/deta**3
+        x_eta3[1] = -2.5*x[1] + 9.0*x[2] - 12.0*x[3] + 7.0*x[4] - 1.5*x[5] 
+        x_eta3[-2] = 2.5*x[-2] - 9.0*x[-3] + 12.0*x[-4] - 7.0*x[-5] + 1.5*x[-6] 
+        
+        u_x = self.calc_first_der(u)
+        u_xx = self.calc_second_der(u)
+        u_xxx = self.calc_third_der(u)
+
+        u_eta4[2:-2] = (u[0:-4] - 4.0*u[1:-3] + 6.0*u[2:-2] - 4.0*u[3:-1] + u[4:])/deta**4
+        if self.hbc:
+            ub = 4.0*(-5.0/6.0*u[0] + 1.5*u[1] - 0.5*u[2] + 1.0/12.0*u[3])
+        else:
+            ub = u[1]
+        u_eta4[1] = (ub - 4.0*u[0] + 6.0*u[1] - 4.0*u[2] + u[3])/deta**4
+        if self.hbc:
+            ub = -4.0*(5.0/6.0*u[-1] - 1.5*u[-2] + 0.5*u[-3] - 1.0/12.0*u[-4])
+        else:
+            ub = u[-2]
+        u_eta4[-2] = (u[-4] - 4.0*u[-3] + 6.0*u[-2] - 4.0*u[-1] + ub)/deta**4
+        
+        x_eta4[2:-2] = (x[0:-4] - 4.0*x[1:-3] + 6.0*x[2:-2] - 4.0*x[3:-1] + x[4:])/deta**4
+        x_eta4[1] = 3.0*x[1] - 14.0*x[2] + 26.0*x[3] - 24.0*x[4] + 11.0*x[5] - 2*x[6] 
+        x_eta4[-2] = -3.0*x[-2] + 14.0*x[-3] - 26.0*x[-4] + 24.0*x[-5] - 11.0*x[-6] + 2*x[-7] 
+        u_xxxx = -(u_x*x_eta4 + 4.0*u_xx*x_eta*x_eta3 + 3.0*u_xx*x_eta2**2 + 6.0*u_xxx*x_eta**2*x_eta2 - u_eta4)/(x_eta**4 + 1e-16)
+        self.u_xxxx[1:-1] = u_xxxx[1:-1]
+        return self.u_xxxx
 
     def parse_equation(self):
         if hasattr(self.equation, "param"):
